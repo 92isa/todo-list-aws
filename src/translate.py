9910ddc3bcddd27698a952d2d8b6a098cb5a6963
Ubuntu <ubuntu@ip-172-31-15-242.ec2.internal>
2023-01-31 18:46:50 +0000

    Changes to be committed: 	modified:   src/translate.py

--- src/translate.py
+++ src/translate.py
@@ -1,20 +1,31 @@
+import os
 import json
-import todoList
+import boto3
+
+from utils import decimalencoder
+from utils.todoTableClass import todoTableClass
+
+
+if os.environ["ENVIRONMENT"] == "LOCAL":
+    dynamodb = None
+else:
+    dynamodb = boto3.resource("dynamodb")
 
 
 def translate(event, context):
-    # create a response
-    item = todoList.get_item(event['pathParameters']['id'])
-    translation = todoList.translate_item(
-        item['text'], event['pathParameters']['language'])
-    if translation:
-        response = {
-            "statusCode": 200,
-            "body": json.dumps(translation)
-        }
-    else:
-        response = {
-            "statusCode": 404,
-            "body": ""
-        }
+    mytable = todoTableClass(dynamodb)
+
+    source_language = 'auto'  # AmazonComprehend autodetecta el lenguaje origen
+    target_language = event['pathParameters']['lang']
+
+    result = mytable.translate_todo(event['pathParameters']['id'],
+                                    source_language,
+                                    target_language)
+
+    response = {
+                "statusCode": 200,
+                "body": json.dumps(result.get('TranslatedText'),
+                                   cls=decimalencoder.DecimalEncoder)
+            }
+
     return response
