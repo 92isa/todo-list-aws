41e0b38f3723092ec35a50e1f199d5bfee09ce4e
Ubuntu <ubuntu@ip-172-31-15-242.ec2.internal>
2023-02-01 09:45:50 +0000

    Changes to be committed: 	modified:   src/todoList.py

--- src/todoList.py
+++ src/todoList.py
@@ -147,6 +147,7 @@ def create_todo_table(dynamodb):
 
     return table
 
+
 def get_translate(text, language, dynamodb=None):  # pragma: no cover
     translate = boto3.client(
         service_name='translate', region_name='us-east-1', use_ssl=True
@@ -160,5 +161,4 @@ def get_translate(text, language, dynamodb=None):  # pragma: no cover
     else:
         translation = result.get('TranslatedText')
         print('Result translate:'+str(translation))
-        
     return translation
