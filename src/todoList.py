2ee28521183c1da6bcc136b4286533e466734e93
Ubuntu <ubuntu@ip-172-31-15-242.ec2.internal>
2023-02-01 09:38:35 +0000

    Changes to be committed: 	modified:   src/todoList.py 	new file:   src/translate.py 	modified:   template.yaml 	modified:   test/unit/TestToDo.py

--- src/todoList.py
+++ src/todoList.py
@@ -146,3 +146,18 @@ def create_todo_table(dynamodb):
         raise AssertionError()
 
     return table
+
+def get_translate(text, language, dynamodb=None):  # pragma: no cover
+    translate = boto3.client(
+        service_name='translate', region_name='us-east-1', use_ssl=True
+    )
+    try:
+        result = translate.translate_text(
+            Text=text, SourceLanguageCode="auto", TargetLanguageCode=language
+        )
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+    else:
+        translation = result.get('TranslatedText')
+        print('Result translate:'+str(translation))
+        return translation
